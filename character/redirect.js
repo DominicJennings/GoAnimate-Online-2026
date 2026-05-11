f6966f58a5e5dde433ce6fd753a6056898793119
Dominic Jennings <149413413+DominicJennings@users.noreply.github.com>
2026-05-10 18:23:44 -0700

    redirect.js

--- character/redirect.js
+++ character/redirect.js
@@ -1,7 +1,22 @@
 const http = require("http");
 const defaultTypes = {
-	family: "adam",
 	anime: "guy",
+	cctoonadventure: "default",
+	family: "adam",
+};
+const bfTypes = {
+	man: "default&ft=_sticky_filter_guy",
+	woman: "default&ft=_sticky_filter_girl",
+	boy: "kid&ft=_sticky_filter_littleboy",
+	girl: "kid&ft=_sticky_filter_littlegirl",
+	heavy_man: "heavy&ft=_sticky_filter_heavyguy",
+	heavy_woman: "heavy&ft=_sticky_filter_heavygirl",
+};
+const wbTypes = {
+	man: "default&ft=_sticky_filter_guy",
+	woman: "default&ft=_sticky_filter_girl",
+	boy: "kid&ft=_sticky_filter_littleboy",
+	girl: "kid&ft=_sticky_filter_littlegirl"
 };
 
 /**
@@ -23,7 +38,9 @@ module.exports = function (req, res, url) {
 			break;
 		}
 		default: {
-			var type = url.query.type || defaultTypes[theme] || "";
+			var type = theme == "business" ?
+					bfTypes[url.query.type || ""] || "": url.query.type | theme == "whiteboard" ?
+					wbTypes[url.query.type || ""] || "": url.query.type || defaultTypes[theme] || "";
 			redirect = `/cc?themeId=${theme}&bs=${type}`;
 			break;
 		}
